4a8856caaf5352f2bb5cf8179f8a77923851ea35
gharri-wat <Gharri.Wat_Course@pm.me>
2024-08-01 17:06:52 -0600

    sample.c
    Use of fseek

--- sample.c
+++ sample.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define BUFSIZE 256
+long get_file_size(const char *filename) {
+    FILE *file = fopen(filename, "rb"); // Open file in binary mode
+    if (file == NULL) {
+        perror("Error opening file");
+        return -1; // Indicate error
+    }
 
-//This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-//Ensure that the user supplied exactly one command line argument
-	if (argc !=2){
-		fprintf(stderr, "Please provide the address of a file as an input.\n");
-		return -1;
-	}
-	char cmd[BUFSIZE] = "wc -c< ";
-	strcat(cmd,argv[1]);
-	system(cmd);
+    // Move to the end of the file
+    if (fseek(file, 0, SEEK_END) != 0) {
+        perror("Error seeking to end of file");
+        fclose(file);
+        return -1; // Indicate error
+    }
+
+    // Get the current position in the file (which is the size of the file)
+    long size = ftell(file);
+    if (size == -1L) {
+        perror("Error telling position in file");
+        fclose(file);
+        return -1; // Indicate error
+    }
+
+    // Close the file
+    fclose(file);
+
+    return size;
+}
+
+int main() {
+    const char *filename = "example.txt";
+    long size = get_file_size(filename);
+
+    if (size >= 0) {
+        printf("Size of file '%s': %ld bytes\n", filename, size);
+    } else {
+        printf("Failed to determine the size of the file.\n");
+    }
+
+    return 0;
 }
